ee58164faedc9d66392f1a9030dd602d681e8456
mhimranhossain <mh.imranhossain@outlook.com>
2023-06-12 23:36:29 +0600

    test 2

--- src/components/canvas/Stars.tsx
+++ src/components/canvas/Stars.tsx
@@ -1,13 +1,13 @@
 import { useState, useRef, Suspense } from "react"
 import { Canvas, useFrame } from "@react-three/fiber"
 import { Points, PointMaterial, Preload } from "@react-three/drei"
-import * as random  from "maath/random/dist/maath-random.esm"
+import * as random from "maath/random"
 
-const Stars = (props:any) => {
+const Stars = (props: any) => {
   const ref = useRef()
   const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }))
 
-  useFrame((state, delta) => {
+  useFrame((state: any, delta: any) => {
     ref.current.rotation.x -= delta / 10
     ref.current.rotation.y -= delta / 15
   })
