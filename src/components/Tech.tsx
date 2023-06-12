67570905d4f6b1de343319aa3caa7c59cfac454a
mhimranhossain <mh.imranhossain@outlook.com>
2023-06-12 23:18:55 +0600

    Update 06/12/2023 23:18:55

--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -1,5 +1,18 @@
-export default function Tech () {
+
+import { BallCanvas } from "./canvas"
+import { SectionWrapper } from "../hoc"
+import { technologies } from "../contents/contents"
+
+const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      {technologies.map((technology) => (
+        <div className='w-28 h-28' key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
+    </div>
   )
 }
+
+export default SectionWrapper(Tech, "")
